2a735028d8286567d4e4633605f9b329738445e4
sprightly1991 <sprightly2007@mail.ru>
2016-03-03 12:49:10 +0300

    Initial commitj

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("hello");
+    }
+  }
+}
+
+
