dbaa0baa483986a41b0998bfc97447f0b0424de4
sprightly1991 <sprightly2007@mail.ru>
2016-03-03 13:01:33 +0200

    mac2

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 
 namespace ConsoleApplication
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("hello");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            int a = 5, b = 6;
+            Console.WriteLine(a + b);
+        }
     }
-  }
 }
 
 
